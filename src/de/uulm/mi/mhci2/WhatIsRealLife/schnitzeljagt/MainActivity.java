e0306a5c4e0969b0f90bfcea2fd9cb8bb30e73f9
Adrian Brückner <adrian.brueckner@uni-ulm.de>
2013-05-16 17:27:19 +0200

    default route erstellen hinzugefügt

--- src/de/uulm/mi/mhci2/WhatIsRealLife/schnitzeljagt/MainActivity.java
+++ src/de/uulm/mi/mhci2/WhatIsRealLife/schnitzeljagt/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		rc = RouteController.generateRouteController(1);
+		rc.getDefaultRoute();
 	}
 
 	@Override
