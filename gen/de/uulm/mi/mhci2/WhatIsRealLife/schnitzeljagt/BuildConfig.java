aa8cb9e0cedfd76156ea47a7e3a0eededc017d0e
Adrian Brückner <adrian.brueckner@uni-ulm.de>
2013-05-16 15:31:34 +0200

    Android Project Started

---
+++ gen/de/uulm/mi/mhci2/WhatIsRealLife/schnitzeljagt/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.uulm.mi.mhci2.WhatIsRealLife.schnitzeljagt;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
